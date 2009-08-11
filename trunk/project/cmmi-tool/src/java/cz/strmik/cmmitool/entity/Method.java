f0ba405c69af0bbdd9bf166c0b41fcdb7793b7b3
strmik <strmik@edfbe84c-f458-11dd-a295-0b7b0935bf94>
2009-08-11 20:50:20 +0000

    #6 Added first step of method creation

--- trunk/project/cmmi-tool/src/java/cz/strmik/cmmitool/entity/Method.java
+++ trunk/project/cmmi-tool/src/java/cz/strmik/cmmitool/entity/Method.java
@@ -7,9 +7,15 @@
  */
 package cz.strmik.cmmitool.entity;
 
+import java.util.Set;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 
 /**
  *
@@ -20,8 +26,159 @@ import javax.persistence.NamedQuery;
 @NamedQueries(
     @NamedQuery(name="Method.findAll", query="SELECT m FROM Method m")
 )
-public class Method extends AcronymEntity {
+public class Method extends AbstractEntity {
+
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    private boolean findingOnTheOrgLevel;
+    private boolean findingOnProcessArea;
+    private boolean findingOnGoalLevel;
+    private boolean findingOnPracticeLevel;
+
+    @OneToMany
+    private Set<Rating> processAreaCapLevel;
+    @OneToMany
+    private Set<Rating> processAreaSatisfaction;
+    @OneToMany
+    private Set<Rating> goalSatisfaction;
+    @OneToMany
+    private Set<Rating> orgMaturityLevel;
+
+    @Transient
+    private boolean rateProcessAreaCapLevel;
+    @Transient
+    private boolean rateProcessAreaSatisfaction;
+    @Transient
+    private boolean rateGoalSatisfaction;
+    @Transient
+    private boolean rateOrgMaturityLevel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isFindingOnGoalLevel() {
+        return findingOnGoalLevel;
+    }
+
+    public void setFindingOnGoalLevel(boolean findingOnGoalLevel) {
+        this.findingOnGoalLevel = findingOnGoalLevel;
+    }
+
+    public boolean isFindingOnPracticeLevel() {
+        return findingOnPracticeLevel;
+    }
+
+    public void setFindingOnPracticeLevel(boolean findingOnPracticeLevel) {
+        this.findingOnPracticeLevel = findingOnPracticeLevel;
+    }
+
+    public boolean isFindingOnProcessArea() {
+        return findingOnProcessArea;
+    }
+
+    public void setFindingOnProcessArea(boolean findingOnProcessArea) {
+        this.findingOnProcessArea = findingOnProcessArea;
+    }
+
+    public boolean isFindingOnTheOrgLevel() {
+        return findingOnTheOrgLevel;
+    }
+
+    public void setFindingOnTheOrgLevel(boolean findingOnTheOrgLevel) {
+        this.findingOnTheOrgLevel = findingOnTheOrgLevel;
+    }
+
+    public Set<Rating> getGoalSatisfaction() {
+        return goalSatisfaction;
+    }
+
+    public void setGoalSatisfaction(Set<Rating> goalSatisfaction) {
+        this.goalSatisfaction = goalSatisfaction;
+    }
+
+    public Set<Rating> getOrgMaturityLevel() {
+        return orgMaturityLevel;
+    }
+
+    public void setOrgMaturityLevel(Set<Rating> orgMaturityLevel) {
+        this.orgMaturityLevel = orgMaturityLevel;
+    }
+
+    public Set<Rating> getProcessAreaCapLevel() {
+        return processAreaCapLevel;
+    }
+
+    public void setProcessAreaCapLevel(Set<Rating> processAreaCapLevel) {
+        this.processAreaCapLevel = processAreaCapLevel;
+    }
+
+    public Set<Rating> getProcessAreaSatisfaction() {
+        return processAreaSatisfaction;
+    }
+
+    public void setProcessAreaSatisfaction(Set<Rating> processAreaSatisfaction) {
+        this.processAreaSatisfaction = processAreaSatisfaction;
+    }
+
+    public boolean isRateGoalSatisfaction() {
+        return rateGoalSatisfaction;
+    }
+
+    public void setRateGoalSatisfaction(boolean rateGoalSatisfaction) {
+        this.rateGoalSatisfaction = rateGoalSatisfaction;
+    }
+
+    public boolean isRateOrgMaturityLevel() {
+        return rateOrgMaturityLevel;
+    }
+
+    public void setRateOrgMaturityLevel(boolean rateOrgMaturityLevel) {
+        this.rateOrgMaturityLevel = rateOrgMaturityLevel;
+    }
+
+    public boolean isRateProcessAreaCapLevel() {
+        return rateProcessAreaCapLevel;
+    }
+
+    public void setRateProcessAreaCapLevel(boolean rateProcessAreaCapLevel) {
+        this.rateProcessAreaCapLevel = rateProcessAreaCapLevel;
+    }
+
+    public boolean isRateProcessAreaSatisfaction() {
+        return rateProcessAreaSatisfaction;
+    }
+
+    public void setRateProcessAreaSatisfaction(boolean rateProcessAreaSatisfaction) {
+        this.rateProcessAreaSatisfaction = rateProcessAreaSatisfaction;
+    }
 
     @Override
     public int hashCode() {
@@ -47,5 +204,5 @@ public class Method extends AcronymEntity {
     public String toString() {
         return "cz.strmik.cmmitool.entity.Method[id=" + id + "]";
     }
-
+    
 }
